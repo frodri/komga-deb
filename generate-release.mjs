b3d2869fdb7268a0a7ca68df07e6ee24292cbecd
Francisco Rodriguez <872826+frodri@users.noreply.github.com>
2023-11-18 18:00:23 -0500

    Workflows have been refactored to use node scripts.

---
+++ generate-release.mjs
@@ -0,0 +1,36 @@
+import { readFile } from 'node:fs/promises';
+import { exec as execImpl } from "node:child_process";
+import { promisify } from "node:util";
+
+const exec = promisify(execImpl);
+
+const main = async () => {
+    const versionFile = await readFile('current.txt');
+    const version = versionFile.toString();
+
+    await exec(
+        'git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"'
+    );
+
+    await exec(
+        'git config --global user.name "github-actions[bot]"'
+    );
+
+    await exec(
+        'git add current.txt'
+    );
+
+    await exec(
+        `git commit -m "Komga version updated to ${version}."`
+    );
+
+    await exec(
+        'git push origin master'
+    );
+
+    await exec(
+        `gh release create ${version} deb/*.deb`
+    );
+}
+
+main().catch(console.error);
