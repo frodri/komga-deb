bb629b84d9269c37cf3eb4155788d1005f00d527
Francisco Rodriguez <872826+frodri@users.noreply.github.com>
2024-04-12 17:04:00 -0400

    Fix for incorrect user:group on deb files.

--- generate-debs.mjs
+++ generate-debs.mjs
@@ -1,84 +1,88 @@
 import { readFile, rm, mkdir, copyFile, chmod, writeFile } from 'node:fs/promises';
-import { exec as execImpl } from "node:child_process";
-import { promisify } from "node:util";
+import { exec as execImpl } from 'node:child_process';
+import { promisify } from 'node:util';
+import { setgid, setuid } from 'node:process';
 
 const exec = promisify(execImpl);
 
 const main = async () => {
-    // Look up release data.
-    const releaseLookup = await fetch('https://api.github.com/repos/gotson/komga/releases/latest');
-    const releaseData = await releaseLookup.json();
+  setuid('root');
+  setgid('root');
 
-    const newVersion = releaseData.tag_name;
+  // Look up release data.
+  const releaseLookup = await fetch('https://api.github.com/repos/gotson/komga/releases/latest');
+  const releaseData = await releaseLookup.json();
 
-    // Compare versions.
-    const versionFile = await readFile('current.txt');
-    if (versionFile.toString() === newVersion) {
-        console.log('Version match. Exiting.');
-        return;
-    }
+  const newVersion = releaseData.tag_name;
 
-    // Ensure that no output from an older build remains.
-    const workingDirs = ['deb', 'tmp'];
-    const dirRemovals = workingDirs.map(
-        (dir) => rm(dir, {recursive: true, force:true})
+  // Compare versions.
+  const versionFile = await readFile('current.txt');
+  if (versionFile.toString() === newVersion) {
+    console.log('Version match. Exiting.');
+    return;
+  }
+
+  // Ensure that no output from an older build remains.
+  const workingDirs = ['deb', 'tmp'];
+  const dirRemovals = workingDirs.map(
+    (dir) => rm(dir, { recursive: true, force: true })
+  );
+  await Promise.all(dirRemovals);
+
+  await mkdir('deb', { mode: 0o755 });
+
+  // Download the binary while we prepare the rest of the package.
+  await mkdir('tmp/usr/share/java', { recursive: true, mode: 0o750 });
+  const binaryRequest = fetch(
+    `https://github.com/gotson/komga/releases/download/${newVersion}/komga-${newVersion}.jar`
+  );
+  const binaryResponse = await binaryRequest;
+  const binaryBuffer = Buffer.from(await binaryResponse.arrayBuffer());
+  const binaryWriter = writeFile('tmp/usr/share/java/komga.jar', binaryBuffer, { mode: 0o755 });
+
+  // Prep the DEBIAN dir.
+  await mkdir('tmp/DEBIAN', { recursive: true, mode: 0o755 });
+  const DEBIANfiles = ['postinst', 'postrm', 'preinst', 'prerm'];
+  const DEBIANfileCopies = DEBIANfiles.map(
+    (file) => copyFile(`DEBIAN/${file}`, `tmp/DEBIAN/${file}`)
+  );
+  await Promise.all(DEBIANfileCopies);
+  const DEBIANfileChmods = DEBIANfiles.map(
+    (file) => chmod(`tmp/DEBIAN/${file}`, 0o755)
+  );
+  await Promise.all(DEBIANfileChmods);
+
+  // Copy our systemd service.
+  await mkdir('tmp/etc/systemd/system', { recursive: true, mode: 0o750 });
+  await copyFile('komga.service', 'tmp/etc/systemd/system/komga.service');
+  await chmod('tmp/etc/systemd/system/komga.service', 0o755);
+
+  // Create a blank /var/lib/ directory.
+  await mkdir('tmp/var/lib/komga', { recursive: true, mode: 0o750 });
+
+  // Wait for the binary download to finish.
+  await binaryWriter;
+
+  const DEBIANcontrolFile = await readFile('DEBIAN/control');
+  const DEBIANcontrolData = DEBIANcontrolFile.toString();
+  const architectures = ['all'];
+
+  // Loop through the architectures and build each one.
+  for (const architecture of architectures) {
+    let editedControlData = DEBIANcontrolData.replaceAll(
+      'VERSION-TO-REPLACE',
+      newVersion
     );
-    await Promise.all(dirRemovals);
-
-    await mkdir('deb', {mode: 0o755});
-
-    // Download the binary while we prepare the rest of the package.
-    await mkdir('tmp/usr/share/java', {recursive: true, mode: 0o750});
-    const binaryRequest = fetch(
-        `https://github.com/gotson/komga/releases/download/${newVersion}/komga-${newVersion}.jar`
+    editedControlData = editedControlData.replaceAll(
+      'ARCHI-TO-REPLACE',
+      architecture
     );
-    const binaryResponse = await binaryRequest;
-    const binaryBuffer = Buffer.from(await binaryResponse.arrayBuffer());
-    const binaryWriter = writeFile('tmp/usr/share/java/komga.jar', binaryBuffer, {mode: 0x755});
+    await writeFile('tmp/DEBIAN/control', editedControlData, { mode: 0o755 });
+    await exec('dpkg-deb --build tmp deb');
+  }
 
-    // Prep the DEBIAN dir.
-    await mkdir('tmp/DEBIAN', {recursive: true, mode: 0o755});
-    const DEBIANfiles = ['postinst', 'postrm', 'preinst', 'prerm'];
-    const DEBIANfileCopies = DEBIANfiles.map(
-        (file) => copyFile(`DEBIAN/${file}`, `tmp/DEBIAN/${file}`)
-    );
-    await Promise.all(DEBIANfileCopies);
-    const DEBIANfileChmods =  DEBIANfiles.map(
-        (file) => chmod(`tmp/DEBIAN/${file}`, 0o755)
-    );
-    await Promise.all(DEBIANfileChmods);
-
-    // Copy our systemd service.
-    await mkdir('tmp/etc/systemd/system', {recursive: true, mode: 0o750});
-    await copyFile('komga.service', 'tmp/etc/systemd/system/komga.service');
-    await chmod('tmp/etc/systemd/system/komga.service', 0o755);
-
-    // Create a blank /var/lib/ directory.
-    await mkdir('tmp/var/lib/komga', {recursive: true, mode: 0o750})
-
-    // Wait for the binary download to finish.
-    await binaryWriter;
-
-    const DEBIANcontrolFile = await readFile('DEBIAN/control');
-    const DEBIANcontrolData = DEBIANcontrolFile.toString();
-    const architectures = ['all'];
-
-    // Loop through the architectures and build each one.
-    for (const architecture of architectures) {
-        let editedControlData = DEBIANcontrolData.replaceAll(
-            'VERSION-TO-REPLACE',
-            newVersion
-        );
-        editedControlData = editedControlData.replaceAll(
-            'ARCHI-TO-REPLACE',
-            architecture
-        );
-        await writeFile('tmp/DEBIAN/control', editedControlData, {mode: 0o755});
-        await exec('dpkg-deb --build tmp deb');
-    }
-
-    // Update the version number.
-    await writeFile('current.txt', newVersion);
-}
+  // Update the version number.
+  await writeFile('current.txt', newVersion);
+};
 
 main().catch(console.error);
--- generate-release.mjs
+++ generate-release.mjs
@@ -1,42 +1,46 @@
 import { readFile } from 'node:fs/promises';
 import { existsSync } from 'node:fs';
-import { exec as execImpl } from "node:child_process";
-import { promisify } from "node:util";
+import { exec as execImpl } from 'node:child_process';
+import { promisify } from 'node:util';
+import { setgid, setuid } from 'node:process';
 
 const exec = promisify(execImpl);
 
 const main = async () => {
-    if (!existsSync('deb')) {
-        console.log('No pending releases. Exiting.');
-        return;
-    }
+  setuid('root');
+  setgid('root');
 
-    const versionFile = await readFile('current.txt');
-    const version = versionFile.toString();
+  if (!existsSync('deb')) {
+    console.log('No pending releases. Exiting.');
+    return;
+  }
 
-    await exec(
-        'git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"'
-    );
+  const versionFile = await readFile('current.txt');
+  const version = versionFile.toString();
 
-    await exec(
-        'git config --global user.name "github-actions[bot]"'
-    );
+  await exec(
+    'git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"'
+  );
 
-    await exec(
-        'git add current.txt'
-    );
+  await exec(
+    'git config --global user.name "github-actions[bot]"'
+  );
 
-    await exec(
-        `git commit -m "Komga version updated to ${version}."`
-    );
+  await exec(
+    'git add current.txt'
+  );
 
-    await exec(
-        'git push origin master'
-    );
+  await exec(
+    `git commit -m "Komga version updated to ${version}."`
+  );
 
-    await exec(
-        `gh release create ${version} deb/*.deb`
-    );
-}
+  await exec(
+    'git push origin master'
+  );
+
+  await exec(
+    `gh release create ${version} deb/*.deb`
+  );
+};
 
 main().catch(console.error);
